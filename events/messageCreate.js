2bd70650004561b141aed502d2c51718bd6610d3
Scienceyzac <scienceyzac@gmail.com>
2021-09-30 18:09:52 +1000

    added a default prefix

---
+++ events/messageCreate.js
@@ -0,0 +1,8 @@
+const { prefix } = require('../config.json')
+module.exports = {
+    name: 'messageCreate',
+    run: (message) => {
+        if (message.author.bot) return;
+        if (message)
+    }
+};
