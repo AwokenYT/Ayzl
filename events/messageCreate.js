d81c0d7e1895328a6d69813e0e46802ab4dd02d5
Scienceyzac <scienceyzac@gmail.com>
2021-09-30 20:17:06 +1000

    added the message event

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,16 @@
-const { prefix } = require('../config.json')
+const { prefix } = require('../config.json');
 module.exports = {
     name: 'messageCreate',
-    run: (message) => {
+    run: (message, client) => {
         if (message.author.bot) return;
-        if (message)
+        let args;
+        if (message.content.startsWith(prefix)) {// if it isnt a cmd then check other stuff, e.g. automod
+            args = message.content.slice(2).split(' ');
+            const possibleCommand = args.shift();
+            let command = client.commands.get(possibleCommand);
+            if (!command) command = client.commands.find(cmd => cmd.aliases?.includes(possibleCommand));
+            if (!command) return;
+            command.run(client, message, args);
+        } else return; // doesnt do anything for now
     }
 };
