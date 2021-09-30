dfb5228b36b94e6e7811e73e5245016868542d9d
Scienceyzac <scienceyzac@gmail.com>
2021-09-30 20:15:23 +1000

    added the command collection

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const {Client, Intents} = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
 const { readdirSync } = require('fs');
-
+const {Client, Intents, Collection} = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+client.commands = new Collection();
 readdirSync('./handlers').forEach(handler => {
     require(`./handlers/${handler}`)(client);
 });
