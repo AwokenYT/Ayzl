7439b473ad08c368c368c44ad1c00af6ed604687
Scienceyzac <scienceyzac@gmail.com>
2021-09-30 18:37:02 +1000

    added comments

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     once: true, 
     run: (client) => {
-        console.log(`Bot is online and running ${client.user.tag}\nWith ${client.guilds.cache.size} guilds and ${client.users.cache.size} users cached!!`);
+        console.log(`Bot is online and running ${client.user.tag}\nWith ${client.guilds.cache.size} guilds and ${client.users.cache.size} users cached!!`);// info on the bot
     }
 };
