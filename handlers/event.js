c402d71a4d5cc2a0395cabd293deb34d2a6a82b0
Scienceyzac <scienceyzac@gmail.com>
2021-09-30 17:21:21 +1000

    added comments

--- handlers/event.js
+++ handlers/event.js
@@ -2,7 +2,7 @@ const { readdirSync } = require('fs');
 module.exports = (client) => {
     readdirSync('./events').forEach(eventFileName => {
         const event = require(`../events/${eventFileName}`);
-        if (event.once) client.once(event.name, (...args) => event.run(...args)); // if the event should only be run once
-        else client.on(event.name, (...args) => event.run(...args));
+        if (event.once) client.once(event.name, (...args) => event.run(...args, client)); // if the event should only be run once
+        else client.on(event.name, (...args) => event.run(...args, client));
     });
 };
