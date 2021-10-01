d1e151f4b06822c7d22cd35906358bf07ba67b82
Scienceyzac <scienceyzac@gmail.com>
2021-10-01 15:24:40 +1000

    added ownerOnly for owner only commands

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { prefix } = require('../config.json');
+const { prefix, ownerIds } = require('../config.json');
 module.exports = {
     name: 'messageCreate',
     run: (message, client) => {
@@ -10,6 +10,10 @@ module.exports = {
             let command = client.commands.get(possibleCommand);
             if (!command) command = client.commands.find(cmd => cmd.aliases?.includes(possibleCommand));
             if (!command) return;
+            if (command.ownerOnly && !ownerIds.includes(message.author.id)) {
+                if (typeof(command.ownerOnly) == 'string') return message.reply(command.ownerOnly);
+                else return;// for commands where we want 0 response
+            }
             command.run(client, message, args);
         } else return; // doesnt do anything for now
     }
