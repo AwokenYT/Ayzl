232dc1dca204d026817de64b1f5c3cd236758ee6
Scienceyzac <scienceyzac@gmail.com>
2021-10-11 16:01:53 +1000

    finished whois cmd "for now"

--- commands/info/whois.js
+++ commands/info/whois.js
@@ -14,7 +14,7 @@ module.exports = {
                     if (!member) member = members.get(memberString.match(/^<@!?(\d+)>$/)?.[1]);
                 }
                 if (!member) return message.reply('no member found');
-
+                
                 const fields = [
                     {
                         name: 'Joined:',
@@ -25,16 +25,47 @@ module.exports = {
                         name: 'Registered:',
                         value: `${moment(member.user.createdAt).utc().format('MMMM Do YYYY, h:mm a')} UTC Time`,
                         inline: true
-                    }
+                    },
+                    {
+                        name: 'Joined Pos:',
+                        value: `${
+                            message.guild.members.cache.map(guildMember => guildMember).sort((memberOne,memberTwo) => memberOne.joinedTimestamp - memberTwo.joinedTimestamp)
+                            .filter((guildMember,index) => {
+                                guildMember.index = index+1; return guildMember.user.id== member.user.id;
+                            })[0].index
+                        }`,
+                        inline: true
+                    },
+                    {
+                        name: 'Status',
+                        value: member.presence.status.replace(/(?<!\w)\w/, x => x.toUpperCase()).replace('Dnd', 'DND')
+                    },
+                    {
+                        name: 'Device Status\'s',
+                        value: [{device: 'Mobile',status:member.presence.clientStatus.mobile}, {device: 'Web',status:member.presence.clientStatus.web}, {device: 'Desktop',status: member.presence.clientStatus.desktop}].map(status => `${status.device}: ${status.status == undefined ? 'Offline' : status.status.replace(/(?<!\w)\w/, x => x.toUpperCase()).replace('Dnd', 'DND')}`).join(', ')
+                    },
+                    {
+                        name: `Roles [${member.roles.cache.size-1}]`,
+                        value: member.roles.cache.size != 1 ? member.roles.cache.map(role => role).filter(role => role.name != '@everyone').join(', ') : 'No Roles'
+                    },
                 ];
+                if (member.presence.activities.some(activity => activity.type == 'CUSTOM')) {
+                    fields.splice(4,0,{
+                            name: 'Custom Status',
+                            value: member.presence.activities.find(activity => activity.type == 'CUSTOM').state.replace(/:(\w+ ?){1,}:/g, '').replace(/<([0-9]{5,20})>/g, '')
+                        });
+                }
 
                 const userInfo = new MessageEmbed()
                 .setTitle(`${member.user.username}`)
                 .setAuthor(`${member.user.username}`, member.user.displayAvatarURL({dynamic: true}))
+                .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
                 .setURL(`https://discord.com/users/${member.user.id}`)
                 .addFields(fields);
-
-                message.reply({embeds: [userInfo]});
+                member.user.fetch().then(user => {
+                    userInfo.setColor(user.accentColor);
+                    message.reply({embeds: [userInfo]});
+                });
             });
         }
     };
